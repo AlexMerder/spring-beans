90dd010d93209900d97ab3f8ab913a7e2246dde6
AlexMerder <63518412+AlexMerder@users.noreply.github.com>
2021-02-15 23:45:50 +0300

    For checking

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,19 @@
 package app.config;
 
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
+/*
+    @Bean(name = "dog")
+    public Dog getHelloWorld() {
+        Dog dog = new Dog();
+        Dog.setName("Bob");
+        return dog;
+    }*/
 }
